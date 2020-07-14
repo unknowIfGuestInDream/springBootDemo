20097420aa97cb1dd26d7c5cbf8c9b4f3df1b453
唐亮 <tangliang@newangels.cn>
2020-07-14 14:07:34 +0800

    注释activiti配置

--- src/main/java/com/tangl/demo/SpringBootDemoApplication.java
+++ src/main/java/com/tangl/demo/SpringBootDemoApplication.java
@@ -2,7 +2,6 @@ package com.tangl.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 
