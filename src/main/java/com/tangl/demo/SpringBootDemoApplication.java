d0852cf52eb9d2cb18043e0129af1b7dc38fc000
唐亮 <tang97155@163.com>
2020-08-08 01:37:16 +0800

    新增异步注解

--- src/main/java/com/tangl/demo/SpringBootDemoApplication.java
+++ src/main/java/com/tangl/demo/SpringBootDemoApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 //开启工作流 activiti
 //@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, org.activiti.spring.boot.SecurityAutoConfiguration.class})
 @SpringBootApplication
 //@EnableScheduling //对定时任务支持
 @EnableCaching
+@EnableAsync
 @ServletComponentScan(basePackages = {"com.tangl.demo.filter", "com.tangl.demo.listener"})
 public class SpringBootDemoApplication {
 
