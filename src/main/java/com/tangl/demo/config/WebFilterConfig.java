f3f11cee1ed5fbd3e79549436f1cb81d425756a7
唐亮 <tang97155@163.com>
2020-08-20 18:44:07 +0800

    Xss过滤器；

--- src/main/java/com/tangl/demo/config/WebFilterConfig.java
+++ src/main/java/com/tangl/demo/config/WebFilterConfig.java
@@ -1,6 +1,9 @@
 package com.tangl.demo.config;
 
 import com.tangl.demo.filter.MyFilter2;
+import com.tangl.demo.filter.XssFilter;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +11,11 @@ import org.springframework.core.convert.ConversionService;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.format.support.FormattingConversionServiceFactoryBean;
 
+import javax.servlet.DispatcherType;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 配置过滤器
@@ -20,6 +26,12 @@ import java.util.Collections;
  */
 @Configuration
 public class WebFilterConfig {
+    @Value("${xss.enabled}")
+    private String enabled;
+    @Value("${xss.excludes}")
+    private String excludes;
+    @Value("${xss.urlPatterns}")
+    private String urlPatterns;
 
     @Bean
     public ConversionService conversionService() {
@@ -36,6 +48,21 @@ public class WebFilterConfig {
         FilterRegistrationBean registration = new FilterRegistrationBean(new MyFilter2());
         registration.addUrlPatterns("/hello");
         registration.setName("myFilter2");
+        return registration;
+    }
+
+    @Bean
+    public FilterRegistrationBean xssFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setDispatcherTypes(DispatcherType.REQUEST);
+        registration.setFilter(new XssFilter());
+        registration.addUrlPatterns(StringUtils.split(urlPatterns, ","));
+        registration.setName("xssFilter");
+        registration.setOrder(Integer.MAX_VALUE);
+        Map<String, String> initParameters = new HashMap<String, String>();
+        initParameters.put("excludes", excludes);
+        initParameters.put("enabled", enabled);
+        registration.setInitParameters(initParameters);
         return registration;
     }
 }
---
+++ src/main/java/com/tangl/demo/filter/XssHttpServletRequestWrapper.java
@@ -0,0 +1,37 @@
+package com.tangl.demo.filter;
+
+import com.tangl.demo.util.html.EscapeUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+/**
+ * XSS过滤处理
+ *
+ * @author ruoyi
+ */
+public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
+    /**
+     * @param request
+     */
+    public XssHttpServletRequestWrapper(HttpServletRequest request) {
+        super(request);
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+        String[] values = super.getParameterValues(name);
+        if (values != null) {
+            int length = values.length;
+            String[] escapseValues = new String[length];
+            for (int i = 0; i < length; i++) {
+                System.out.println(values[i]);
+                // 防xss攻击和过滤前后空格
+                escapseValues[i] = EscapeUtil.clean(values[i]).trim();
+                System.out.println(escapseValues[i]);
+            }
+            return escapseValues;
+        }
+        return super.getParameterValues(name);
+    }
+}
