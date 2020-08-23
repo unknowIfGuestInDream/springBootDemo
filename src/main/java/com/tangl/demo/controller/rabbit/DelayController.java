b18d1fda53db073df46f0ef1b746d3a62b1f7a32
唐亮 <tang97155@163.com>
2020-08-23 20:48:11 +0800

    代码格式化

--- src/main/java/com/tangl/demo/controller/rabbit/DelayController.java
+++ src/main/java/com/tangl/demo/controller/rabbit/DelayController.java
@@ -1,17 +1,11 @@
 package com.tangl.demo.controller.rabbit;
 
-import com.alibaba.fastjson.JSONObject;
+import com.tangl.demo.annotation.LogAnno;
 import com.tangl.demo.rabbitMQ.delay.RabbitMQConfiguration;
+import com.tangl.demo.rabbitMQ.topic.TopicSender;
 import com.tangl.demo.util.DateUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.http.ParseException;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -20,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Date;
 
@@ -38,15 +31,28 @@ import java.util.Date;
 public class DelayController {
     @Autowired
     private RabbitTemplate rabbitTemplate;
+    @Autowired
+    private TopicSender topicSender;
 
     @ApiOperation("延迟消息")
+    @LogAnno(operateType = "延迟消息接口")
     @RequestMapping(value = "/delayTest", method = RequestMethod.GET)
-    public String delayTest() throws URISyntaxException {
+    public String delayTest() {
         MessageProperties properties = new MessageProperties();
         properties.setDelay(10000);
         Message message = new Message("delay_test_message".getBytes(), properties);
         System.out.println(DateUtils.parseDateToStr(DateUtils.YYYY_MM_DD_HH_MM_SS, new Date()));
         rabbitTemplate.send(RabbitMQConfiguration.EXCHANGE_NAME, RabbitMQConfiguration.QUEUE_NAME, message);
+        return null;
+    }
+
+    @ApiOperation("topic消息")
+    @LogAnno(operateType = "topic消息接口")
+    @RequestMapping(value = "/topicTest", method = RequestMethod.GET)
+    public String topicTest() throws InterruptedException {
+        topicSender.send1();
+        Thread.sleep(1000);
+        topicSender.send2();
         return null;
     }
 }
