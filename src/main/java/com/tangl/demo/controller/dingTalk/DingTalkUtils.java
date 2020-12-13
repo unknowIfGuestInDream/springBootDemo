e40f3fe4d3e701bbcb6ede96fc3c794744f981fd
唐亮 <tang97155@163.com>
2020-12-13 11:07:00 +0800

    DingTalkUtils测试&注释
    Signed-off-by: 唐亮 <tang97155@163.com>

--- src/main/java/com/tangl/demo/controller/dingTalk/DingTalkUtils.java
+++ src/main/java/com/tangl/demo/controller/dingTalk/DingTalkUtils.java
@@ -31,6 +31,7 @@ public class DingTalkUtils {
     @Value("${dingTalk.webhook-token}")
     private String webhook;
 
+    //钉钉开发文档https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq/e9d991e2
     public OapiRobotSendResponse sendMsg(String msg) throws ApiException, NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException {
         Long timestamp = System.currentTimeMillis();
         String secret = "SEC903221b6570a47d0105a16162b9ceca0812f1a3d6c6e70edad69de779b7a6693";
@@ -41,35 +42,39 @@ public class DingTalkUtils {
         byte[] signData = mac.doFinal(stringToSign.getBytes("UTF-8"));
         String sign = URLEncoder.encode(new String(Base64.encodeBase64(signData)), "UTF-8");
 
-        //DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/robot/send?access_token=e5411f292da68dc1bd9e2a56ee619f4d3c4b1b84ca8244d6749fb9ecb0d0de01");
-        DingTalkClient client = new DefaultDingTalkClient(webhook + "&timestamp=" + timestamp + "&sign=" + sign);
+        DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/robot/send?access_token=e5411f292da68dc1bd9e2a56ee619f4d3c4b1b84ca8244d6749fb9ecb0d0de01");
+        //DingTalkClient client = new DefaultDingTalkClient(webhook + "&timestamp=" + timestamp + "&sign=" + sign);
         OapiRobotSendRequest request = new OapiRobotSendRequest();
+
+        //text
         request.setMsgtype("text");
         OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
         text.setContent("业务报警:\n" + msg);
         request.setText(text);
+
         OapiRobotSendRequest.At at = new OapiRobotSendRequest.At();
         //at.setAtMobiles(Arrays.asList("15566418409"));
         at.setIsAtAll(true);
         request.setAt(at);
 
+        //link
         request.setMsgtype("link");
         OapiRobotSendRequest.Link link = new OapiRobotSendRequest.Link();
         link.setMessageUrl("https://www.dingtalk.com/");
         link.setPicUrl("");
         link.setTitle("时代的火车向前开");
-        link.setText("这个即将发布的新版本，创始人xx称它为红树林。而在此之前，每当面临重大升级，产品经理们都会取一个应景的代号，这一次，为什么是红树林");
+        link.setText("这个即将发布的新版本，创始人xx称它为红树林。而在此之前，每当面临重大升级，产品经理们都会取一个应景的代号，这一次，为什么是红树林 业务报警");
         request.setLink(link);
 
+        //markdown
         request.setMsgtype("markdown");
         OapiRobotSendRequest.Markdown markdown = new OapiRobotSendRequest.Markdown();
         markdown.setTitle("杭州天气");
-        markdown.setText("#### 杭州天气 @156xxxx8827\n" +
+        markdown.setText("业务报警 #### 杭州天气 @156xxxx8827\n" +
                 "> 9度，西北风1级，空气良89，相对温度73%\n\n" +
-                "> ![screenshot](https://gw.alicdn.com/tfs/TB1ut3xxbsrBKNjSZFpXXcXhFXa-846-786.png)\n"  +
+                "> ![screenshot](https://gw.alicdn.com/tfs/TB1ut3xxbsrBKNjSZFpXXcXhFXa-846-786.png)\n" +
                 "> ###### 10点20分发布 [天气](http://www.thinkpage.cn/) \n");
         request.setMarkdown(markdown);
-
 
         OapiRobotSendResponse response = client.execute(request);
         log.info("钉钉机器人发送消息【{}】", response.getErrmsg());
