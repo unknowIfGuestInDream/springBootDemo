36bfdbd2832404bf589440f87d1d549229524fe9
唐亮 <tang97155@163.com>
2021-01-01 13:51:15 +0800

    style(加密解密测试): 代码格式化

--- src/test/java/com/tangl/demo/jasypt/PasswordTest.java
+++ src/test/java/com/tangl/demo/jasypt/PasswordTest.java
@@ -23,13 +23,23 @@ public class PasswordTest {
     @Test
     public void testGeneratePassword() {
         // 你的邮箱密码
-        String password = "ELACZHTZLMLVJYDF";
+        String password = "";
         // 加密后的密码(注意：配置上去的时候需要加 ENC(加密密码))
         String encryptPassword = stringEncryptor.encrypt(password);
         String decryptPassword = stringEncryptor.decrypt(encryptPassword);
 
         System.out.println("password = " + password);
         System.out.println("encryptPassword = " + encryptPassword);
+        System.out.println("decryptPassword = " + decryptPassword);
+    }
+
+    /**
+     * 解密
+     */
+    @Test
+    public void testdecryptPassword() {
+        String encryptPassword = "";
+        String decryptPassword = stringEncryptor.decrypt(encryptPassword);
         System.out.println("decryptPassword = " + decryptPassword);
     }
 }
