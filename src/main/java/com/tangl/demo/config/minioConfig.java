ec24f82ddd0692e1e83a5373278bb601a6dac312
唐亮 <tang97155@163.com>
2021-02-21 21:25:25 +0800

    fix(minioConfig): minioConfig配置更新

--- src/main/java/com/tangl/demo/config/minioConfig.java
+++ src/main/java/com/tangl/demo/config/minioConfig.java
@@ -1,8 +1,6 @@
 package com.tangl.demo.config;
 
 import io.minio.MinioClient;
-import io.minio.errors.InvalidEndpointException;
-import io.minio.errors.InvalidPortException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,13 +22,6 @@ public class minioConfig {
 
     @Bean
     public MinioClient minioClient() {
-        try {
-            return new MinioClient(ENDPOINT, ACCESS_KEY, SECRET_KEY);
-        } catch (InvalidEndpointException e) {
-            e.printStackTrace();
-        } catch (InvalidPortException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return new MinioClient(ENDPOINT, ACCESS_KEY, SECRET_KEY);
     }
 }
